33937a63059e1e4e4123f4a2556f116b0c8710d8
stack-trace <igorsap@operamail.com>
2014-09-22 11:03:28 +0400

    Tests for SharedResource dereferencing

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,13 +6,13 @@
 
 BOOST_AUTO_TEST_CASE(Basic_construction)
 {
-    SharedResource<int> shared_int;
+    SharedResource<int> shared_test_class;
 }
 
 
 BOOST_AUTO_TEST_CASE(Construction_with_argument)
 {
-    SharedResource<int> shared_int(5);
+    SharedResource<int> shared_test_class(5);
 }
 
 
@@ -30,25 +30,54 @@ BOOST_AUTO_TEST_CASE(Construction_no_default_constructor)
 
 BOOST_AUTO_TEST_CASE(Basic_Locking)
 {
-    SharedResource<int> shared_int(0);
+    SharedResource<int> shared_test_class(0);
     shared_int.lock();
 }
 
 
 BOOST_AUTO_TEST_CASE(Locking_with_accessor)
 {
-    SharedResource<int> shared_int(0);
-    auto shared_int_accessor = shared_int.lock();
+    SharedResource<int> shared_test_class(0);
+    auto shared_int_accessor = shared_test_class.lock();
 }
 
 
 BOOST_AUTO_TEST_CASE(Accessor_isValid)
 {
-    SharedResource<int> shared_int(0);
-    auto shared_int_accessor = shared_int.lock();
+    SharedResource<int> shared_test_class(0);
+    auto shared_int_accessor = shared_test_class.lock();
     BOOST_CHECK(shared_int_accessor.isValid());
     auto shared_int_accessor_new(std::move(shared_int_accessor));
     BOOST_CHECK(!shared_int_accessor.isValid());
     BOOST_CHECK(shared_int_accessor_new.isValid());
+}
+
+
+BOOST_AUTO_TEST_CASE(Acessor_dereferencing_1)
+{
+    SharedResource<int> shared_test_class(0);
+    auto shared_int_accessor = shared_test_class.lock();
+    BOOST_CHECK_EQUAL(0, *shared_int_accessor);
+    *shared_int_accessor = 5;
+    BOOST_CHECK_EQUAL(5, *shared_int_accessor);
+}
+
+
+BOOST_AUTO_TEST_CASE(Acessor_dereferencing_2)
+{
+    struct TestClass
+    {
+        TestClass(int a) : m_a(a) { }
+        void set(int a) noexcept { m_a = a; }
+        int get() const noexcept { return m_a; }
+    private:
+        int m_a;
+    };
+
+    SharedResource<TestClass> shared_test_class(0);
+    auto shared_int_accessor = shared_test_class.lock();
+    BOOST_CHECK_EQUAL(0, shared_int_accessor->get());
+    shared_int_accessor->set(5);
+    BOOST_CHECK_EQUAL(5, shared_int_accessor->get());
 }
 
